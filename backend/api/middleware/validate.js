5173aa188f9d0916d11cea54d45757077731a801
Sujal-Tuladhar <tuladhar.sujal10@gmail.com>
2025-03-27 23:59:44 +0545

    Backedn Connected with Mongodb and middleware made

---
+++ backend/api/middleware/validate.js
@@ -0,0 +1,49 @@
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const validate = async (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  console.log(authHeader);
+
+  if (token == null) return res.status(401).send("Unauthorized");
+  jwt.verify(token, process.env.JWT_KEY, async (err, user) => {
+    if (err) return res.status(401).send("Unauthorized");
+    console.log(user);
+    console.log("asdasd");
+
+    user = user._id;
+
+    console.log(user);
+
+    const userData = await User.findById(user).lean();
+    console.log(userData);
+
+    if (!userData) {
+      return res.status(401).send("Unauthorized");
+    }
+
+    req.user = userData;
+    next();
+  });
+};
+
+const validateAdmin = async (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.status(401).send("Unauthorized");
+
+  jwt.verify(token, process.env.JWT_KEY, async (err, user) => {
+    if (err) return res.status(401).send("Unauthorized");
+    const userData = await User.findOne({ _id: user }).lean();
+    if (!userData || userData.type !== "admin") {
+      return res.status(401).send("Unauthorized");
+    }
+
+    req.user = userData;
+    next();
+  });
+};
+
+export { validate, validateAdmin };
--- backend/api/server.js
+++ backend/api/server.js
@@ -1,1 +1,49 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+dotenv.config();
+const app = express();
+//Conencting Frontend to backend #credinatial true allows to send the cookies,tokens
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+// Parses JSON request bodies into req.body.
+app.use(express.json());
+// Parses cookies into req.cookies.
+app.use(cookieParser());
+
+//Connecting mongoDB to backend
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      writeConcern: { w: 1 },
+    });
+  } catch (error) {
+    console.error(" MongoDB Connection Error:", error.message);
+    process.exit(1);
+  }
+};
+//Listens event from mongoose and trigger the function accordingly
+mongoose.connection.on("connected", () => {
+  console.log("✅ MongoDB Connection Successful");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("❌ MongoDB Connection Error:", err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.warn("⚠️ MongoDB Disconnected");
+});
+//Calling the function to connect to the database
+connectDB();
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+  return res.status(errorStatus).send(errorMessage);
+});
+
+app.listen(7700, () => {
+  console.log("🚀 Server started at http://localhost:7700");
+});
